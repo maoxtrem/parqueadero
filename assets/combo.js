ec6a4e2ada998c05a87af639b8a61433fbc65806
maoxtrem <maoxtrem@gmail.com>
2024-07-14 01:23:53 +0000

    ajustes

--- assets/combo.js
+++ assets/combo.js
@@ -6,9 +6,9 @@ import { combo, comboCascade } from './util';
 document.addEventListener('DOMContentLoaded', async () => {
   //  toastr.error('Please enter a number.', 'Error');
   const combos = [
-    { el: '#pais', url: rutes.pais, defaultValue: 0},
-    { el: '#departamento', url: rutes.departamento, defaultValue: 2 },
-    { el: '#municipio', url: rutes.municipio, defaultValue: 1 },
+    { el: '#pais', url: rutes.pais, defaultValue: 1},
+    { el: '#departamento', url: rutes.departamento, defaultValue: 0},
+    { el: '#municipio', url: rutes.municipio, defaultValue: 0 },
 
   ];
   await comboCascade(combos);
